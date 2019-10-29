42e9f59995f836c3c7568e57d88f931250f5915d
Martin Ras <martin+github@x8x.at>
2019-10-29 21:44:09 +0100

    Work with exit codes instead of grep

--- alpine-php-check.sh
+++ alpine-php-check.sh
@@ -13,14 +13,16 @@ tar -zxf apkindex-community.tar.gz
 
 AVAILABLE_PHP_VERSION=`grep -A1 'P:php7-fpm' APKINDEX | grep -o '[0-9].[0-9].[0-9*]*'`
 
+cd ..
+rm -rf $TMP_DIR
+
 if [ "$CURRENT_PHP_VERSION" = "$AVAILABLE_PHP_VERSION" ];
 then
     echo "Result: OK"
     echo "Available PHP Version is equals current PHP Version: $AVAILABLE_PHP_VERSION = $CURRENT_PHP_VERSION"
+    exit 0
 else
     echo "Result: DIFF"
     echo "Available PHP Version differs from current PHP Version: $AVAILABLE_PHP_VERSION <> $CURRENT_PHP_VERSION"
+    exit 1
 fi
-
-cd ..
-rm -rf $TMP_DIR
